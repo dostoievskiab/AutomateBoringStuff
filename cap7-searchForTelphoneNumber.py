017ae38a2ee87a53b05e52ebb747efd692c48af9
Dostoievski Batista <dostoievskiab@gmail.com>
2017-10-04 10:48:41 -0300

    Changed how to display info

--- cap7-searchForTelphoneNumber.py
+++ cap7-searchForTelphoneNumber.py
@@ -1,7 +1,12 @@
 #Search for pattern (99) 9999-9999 from clipboard then print list with all matchs
 import re,pyperclip
 clipboard = pyperclip.paste()
-print(clipboard)
-condTelefone = re.compile(r"\(\d{2}\)\s\d{4}-\d{4}")
-teste = condTelefone.findall(clipboard)
-print(teste)
+Telefones = re.compile(r"\(\d{2}\)\s\d{4}-\d{4}").findall(clipboard)
+
+for n in range(len(Telefones)):
+    print(Telefones[n])
+
+if len(Telefones) > 0:
+    print('Encontrado ' + str(len(Telefones)) + ' numeros telefonicos.')
+else:
+    print('Verifique se voce copiou (Ctrl+C) algo antes de rodar o programa.')
