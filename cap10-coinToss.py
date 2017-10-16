a125af411822edf7f0b188a769f683b0e4dbded8
Dostoievski Batista <dostoievskiab@gmail.com>
2017-10-16 18:31:26 -0200

    First Commit

---
+++ cap10-coinToss.py
@@ -0,0 +1,21 @@
+import random
+guess = ''
+while guess not in ('heads', 'tails'):
+    print('Guess the coin toss! Enter heads or tails:')
+    guess = input()
+
+toss = random.randint(0, 2)
+if toss == 0:
+    toss = 'tails'
+else:
+    toss = 'heads'
+    
+if toss == guess:
+    print('You got it!')
+else:
+    print('Nope! Guess again!')
+    guess = input()
+    if toss == guess:
+        print('You got it!')
+    else:
+        print('Nope!')
